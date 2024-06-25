4632b867d0e830cea8d025feebfa9e14cede0848
lin_diex <diex@live.cn>
2024-06-25 15:24:04 +0800

    Quartz sync: Jun 25, 2024, 3:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: 'google', 
+      tagId: 'G-VXPHQS9FLJ',
     },
     locale: "en-US",
     baseUrl: "ldiex.github.io/quartz/",
