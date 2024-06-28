4c89129c0ef14f74f6684bc727a10604d593ff3d
lin_diex <diex@live.cn>
2024-06-28 11:38:26 +0800

    Quartz sync: Jun 28, 2024, 11:38 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -65,7 +65,7 @@ const config: QuartzConfig = {
         keepBackground: false,
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
-      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.GitHubFlavoredMarkdown({ enableSmartyPants: false}),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
